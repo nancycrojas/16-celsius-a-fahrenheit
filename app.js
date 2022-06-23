7b4231cbbbaab576f605aa1bc7d95150193c9ec7
nancycrojas <nanclarisaok@gmail.com>
2022-06-23 09:52:59 -0300

    Variables, datos y operadores - ejercicio 16

---
+++ app.js
@@ -0,0 +1,6 @@
+//Celsius a Fahrenheit
+
+//Crear un programa que pida ingresar una cantidad de grados Celsius y muestre un mensaje con el resultado de la conversión a grados Fahrenheit.
+
+const celsius = prompt ("Por favor ingresa los grados Celsius");
+alert("Conversión a Fahrenheit: " + celsius * 33.8)
