fa6730e59f0fad91faf1b75b0a2165fb916934a8
nancycrojas <nanclarisaok@gmail.com>
2022-09-30 02:03:32 -0300

    Agregando variable y template strings

--- app.js
+++ app.js
@@ -2,5 +2,6 @@
 
 //Crear un programa que pida ingresar una cantidad de grados Celsius y muestre un mensaje con el resultado de la conversión a grados Fahrenheit.
 
-const celsius = prompt ("Por favor ingresa los grados Celsius");
-alert("Conversión a Fahrenheit: " + celsius * 33.8)
+const celsius = Number(prompt("Por favor ingresa los grados Celsius"));
+const conversion= (celsius * 1.8) + 32; 
+alert(`La conversión de ${celsius} grados Celsius a grados Fahrenheit es: ${conversion}`);
